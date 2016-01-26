65c7f03adb8c7538991628997cdda18413e58c4b
Jeremy Quijada <jeremy.quijada@gmail.com>
2016-01-25 23:53:32 -0430

    Funcion urls de diego y las mias

--- condominios/apps/administradora/urls.py
+++ condominios/apps/administradora/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, url
 from .views import *
 
 urlpatterns = patterns('',
+
+				url(r'^condominio/guiado$', 'apps.administradora.views.RegistrarCondominioGuiadoView', name='condominioRegistrarGuiado'),
+				url(r'^administradora/registrar_edificacion/(?P<popup>\w{0,50})$', RegistrarTipoEdificacionView.as_view(), name='tipoEdificacionRegistrar'),	
 				url(r'^registrar-usuario$',UsuarioRegistrarView.as_view(),name='usuarioRegistrar'),	
-				url(r'^listar-usuario$',UsuarioListarView.as_view(),name='usuarioListar'),	
-				url(r'^condominio/registrar$',CondominioRegistrarView.as_view(),name='condominioRegistrar'),	
-				url(r'^administradora/proveedor/registrar$', ProveedorRegistrarView.as_view(), name='proveedorRegistrar'),
+				url(r'^listar-usuario$',UsuarioListarView.as_view(),name='usuarioListar'),
 				url(r'^administradora/servicioespecial/registrar$', ServicioEspecialRegistrarView.as_view(), name='servicioEspecialRegistrar'),
 				url(r'^administradora/serviciomensual/registrar$', ServicioMensualRegistrarView.as_view(), name='servicioMensualRegistrar'),
 				url(r'^administradora/costoservicioespecial/registrar$', CostoServicioEspecialRegistrarView.as_view(), name='costoServicioEspecialRegistrar'),
@@ -13,11 +14,24 @@ urlpatterns = patterns('',
 				url(r'^registrar-pago$', 'apps.administradora.views.registrar_pago', name='registrar_pago'),
 				url(r'^relacion-gastos$', 'apps.administradora.views.relacion_gastos', name='relacion_gastos'),
 				url(r'^inicio/$', 'apps.administradora.views.inicio', name='inicio'),
-				url(r'^condominio/$', CondominioListarView.as_view(), name='condominioListar'),						
-				url(r'^condominio/detalle/(?P<pk>\d+)/$', CondominioDetailView.as_view(), name='condominioDetalle'),
-				url(r'^condominio/actualizar/(?P<pk>\d+)/$', CondominioUpdate.as_view(), name='condominioActualizar'),
-				#url(r'^condominio/guiado$', RegistrarCondominioGuiadoView.as_view(), name='condominioRegistrarGuiado'),
-				url(r'^administradora/registrar_edificacion/(?P<popup>\w{0,50})$', RegistrarTipoEdificacionView.as_view(), name='tipoEdificacionRegistrar'),
-				url(r'^condominio/guiado$', 'apps.administradora.views.RegistrarCondominioGuiadoView', name='condominioRegistrarGuiado'),
+				#condominios
+				url(r'^condominio/$', CondominioListarView.as_view(), name='condominioListar'),
+				url(r'^condominio/registrar$',CondominioRegistrarView.as_view(),name='condominioRegistrar'),						
+				url(r'^condominio/detalle/(?P<pk>\d+)/$', CondominioDetalle.as_view(), name='condominioDetalle'),
+				url(r'^condominio/actualizar/(?P<pk>\d+)/$', CondominioActualizar.as_view(), name='condominioActualizar'),
+				url(r'^condominio/eliminar/(?P<pk>\d+)/$', CondominioEliminar.as_view(), name='condominioEliminar'),
+				#proveedores
+				url(r'^proveedor/$', ProveedorListarView.as_view(), name='proveedorListar'),
+				url(r'^proveedor/registrar$', ProveedorRegistrarView.as_view(), name='proveedorRegistrar'),						
+				url(r'^proveedor/detalle/(?P<pk>\d+)/$', ProveedorDetalle.as_view(), name='proveedorDetalle'),
+				url(r'^proveedor/actualizar/(?P<pk>\d+)/$', ProveedorActualizar.as_view(), name='proveedorActualizar'),
+				url(r'^proveedor/eliminar/(?P<pk>\d+)/$', ProveedorEliminar.as_view(), name='proveedorEliminar'),				
+				#servicios mensuales
+				url(r'^servicioMensual/$', ServicioMensualListarView.as_view(), name='servicioMensualListar'),
+				url(r'^servicioMensual/registrar$', ServicioMensualRegistrarView.as_view(), name='servicioMensualRegistrar'),						
+				url(r'^servicioMensual/detalle/(?P<pk>\d+)/$', ServicioMensualDetalle.as_view(), name='servicioMensualDetalle'),
+				url(r'^servicioMensual/actualizar/(?P<pk>\d+)/$', ServicioMensualActualizar.as_view(), name='servicioMensualActualizar'),
+				url(r'^servicioMensual/eliminar/(?P<pk>\d+)/$',ServicioMensualEliminar.as_view(), name='servicioMensualEliminar'),
+
 				
 	)
