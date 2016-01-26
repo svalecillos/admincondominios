6011b6fad0d55fc746311f407ec1734c131af1dc
Jeremy Quijada <jeremy.quijada@gmail.com>
2016-01-26 10:32:51 -0430

    select dependientes de estado, municipio y parroquia

--- condominios/apps/api/urls.py
+++ condominios/apps/api/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = patterns('',
 				url(r'^ajax/prueba$', 'apps.api.views.ApiPrueba', name='ajaxPrueba'),
+				url(r'^ajax/estados$', 'apps.api.views.estados', name='ajaxEstados'),
+				url(r'^ajax/municipios$', 'apps.api.views.municipios', name='ajaxmunicipios'),
+				url(r'^ajax/parroquias$', 'apps.api.views.parroquias', name='ajaxparroquias'),
 				url(r'^ajax/select$', 'apps.api.views.select', name='select'),
+				url(r'^ajax/select_estados$', 'apps.api.views.select_estados', name='select_estados'),
 	)
--- condominios/apps/api/views.py
+++ condominios/apps/api/views.py
@@ -1,13 +1,58 @@
 import json
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from apps.administradora.models import Condominio
+from apps.venezuela.models import *
 
 def ApiPrueba(request):
-	#if request.method == "GET":
+
 	response_data = {}
-	response_data['id'] = 'failed'
-	response_data['literal'] = 'You messed up'
-	return HttpResponse(json.dumps(response_data), content_type="application/json")
+	response_data['id'] =  list(Condominio.objects.values('condominio'))
+	#response_data['id'] =  list(Condominio.objects.values_list('condominio'))
+	response_data['literal'] = ['A','B','L','L','L','L','L','L']
+	if request.is_ajax():
+		return JsonResponse(response_data)
+	else:
+		return JsonResponse(response_data)
+		#return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def estados(request):
+
+	response_data = {}
+	#response_data['id'] =  list(Estado.objects.values('id'))
+	#response_data['estado'] = list(Estado.objects.values('estado'))
+	response_data['resultado'] =  list(Estado.objects.values('id','estado'))
+	if request.is_ajax():
+		return JsonResponse(response_data)
+	else:
+		return JsonResponse(response_data)
+
+def municipios(request):
+
+	if request.is_ajax():
+		response_data = {}
+		response_data['resultado'] =  list(Municipio.objects.values('id','municipio').filter(estado_id=request.POST['elegido']))		
+		return JsonResponse(response_data)
+	else:
+		response_data = {}
+		response_data['resultado'] =  list(Municipio.objects.values('id','municipio').filter(estado_id=request.GET['elegido']))
+		return JsonResponse(response_data)
+
+def parroquias(request):
+
+	if request.is_ajax():
+		response_data = {}
+		response_data['resultado'] =  list(Parroquia.objects.values('id','parroquia').filter(municipio_id=request.POST['elegido']))		
+		return JsonResponse(response_data)
+	else:
+		response_data = {}
+		response_data['resultado'] =  list(Parroquia.objects.values('id','parroquia').filter(municipio_id=request.GET['elegido']))
+		return JsonResponse(response_data)
 
 def select(request):
 	return render(request, 'api/ajax_prueba.html')
+
+def select_estados(request):
+	return render(request, 'api/ajax_select_estados.html')
